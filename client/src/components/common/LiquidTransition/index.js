cd63301222b59fe9494d28556cb6aeb18f989622
akshatkushwaha03 <akshatkushwaha7214@gmail.com>
2026-09-07 13:34:36 +0530

    feat: implement portfolio work archive page with dynamic project routing and category filtering

--- client/src/components/common/LiquidTransition/index.js
+++
@@ -1,4 +0,0 @@
-export { LiquidTransitionProvider } from './LiquidTransitionProvider';
-export { useLiquidTransition } from './useLiquidTransition';
-export { LiquidCanvas } from './LiquidCanvas';
-export { default } from './LiquidTransitionProvider';
